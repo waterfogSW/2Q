f240a8571facdb67b3d90bdfcfe7145a7bff3160
San Kim <waterfog9580@gmail.com>
2021-09-11 22:52:41 +0900

    update

---
+++ include/two_q_cache.hpp
@@ -0,0 +1,136 @@
+#ifndef TWO_Q_CACHE_HPP
+#define TWO_Q_CACHE_HPP
+
+#include "cache.hpp"
+
+namespace cache {
+class TWO_Q : public Cache {
+   public:
+    TWO_Q(size_t size) : Cache(size / 2) {
+        max_sub_csize = size / 2;
+    }
+
+    void Insert(const Key &key, const Value &value);
+    Value Search(const Key &key);
+    // void Erase(const Key &key);
+
+   private:
+    std::list<Node *> sub_que;
+    std::unordered_map<Key, std::list<Node *>::iterator> sub_key_map;
+
+    size_t max_sub_csize;
+    size_t hit_count = 0;
+};
+
+void TWO_Q::Insert(const Key &key, const Value &value) {
+    // Search Am Cash.
+    auto iter = key_map.find(key);
+    Node *node;
+
+    // Am cache hit
+    if (iter != key_map.end()) {
+        que.erase(key_map[key]);
+
+        node = *key_map[key];
+
+        node->value = value;
+        node->v_size = value.size();
+
+        hit_count++;
+
+        que.push_front(node);
+        key_map[key] = que.begin();
+    }
+
+    // Am cache miss
+    else {
+        // Search A1 Cash.
+        auto sub_iter = sub_key_map.find(key);
+        // A1 cache hit
+        if (sub_iter != sub_key_map.end()) {
+            node = *sub_key_map[key];
+
+            node->value = value;
+            node->v_size = value.size();
+
+            hit_count++;
+
+            // erase from A1 cache
+            sub_key_map.erase(sub_iter);
+            sub_que.erase(sub_key_map[key]);
+
+            // push Am cache
+            que.push_front(node);
+            key_map[key] = que.begin();
+        }
+        // A1 cache miss
+        else {
+            if (sub_que.size() >= max_csize) {
+                Node *tmp = sub_que.back();
+                sub_key_map.erase(tmp->key);
+                sub_que.pop_back();
+
+                delete tmp;
+            }
+
+            node = new Node(key, value);
+
+            sub_que.push_front(node);
+            sub_key_map[key] = que.begin();
+        }
+    }
+}
+
+Value TWO_Q::Search(const Key &key) {
+    // Search Am Cash.
+    auto iter = key_map.find(key);
+    Value rvalue;
+    Node *node;
+
+    // Am cache hit
+    if(iter != key_map.end()) {
+        que.erase(key_map[key]);
+
+        node = *key_map[key];
+        rvalue = (*key_map[key])->value;
+
+        hit_count++;
+
+        que.push_front(node);
+        key_map[key] = que.begin();
+        return rvalue;
+    }
+
+    // Am cache miss
+    else {
+        // Search A1 Cash.
+        auto sub_iter = sub_key_map.find(key);
+        // A1 cache hit
+        if(sub_iter != sub_key_map.end()) {
+            sub_que.erase(sub_key_map[key]);
+
+            node = *sub_key_map[key];
+            rvalue = (*sub_key_map[key])->value;
+
+            hit_count++;
+
+            // erase from A1 cache
+            sub_key_map.erase(sub_iter);
+            sub_que.erase(sub_key_map[key]);
+
+            // push Am cache
+            que.push_front(node);
+            key_map[key] = que.begin();
+
+            return rvalue;
+        }
+        // A1 cache miss
+        else {
+            return "";
+        }
+    }
+}
+
+}  // namespace cache
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,11 @@ using namespace std;
 
 #include "lru_cache.hpp"
 #include "fifo_cache.hpp"
+#include "two_q_cache.hpp"
 
 int main() {
-    cache::FIFO f(5);
-    f.Insert("k1","a");
-    f.Insert("k2","a");
-    f.Insert("k3","a");
-    f.Insert("k4","a");
-    f.Insert("k5","a");
-    f.Insert("k6","a");
-    cout << f.Search("k1");
+    cache::TWO_Q a(5);
+
+    a.Insert("key1","val1");
+    a.Search("key1");
 }
